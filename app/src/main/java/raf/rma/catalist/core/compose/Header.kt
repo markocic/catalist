af5fde1037f255e0f3c40f56f966154881ebc8aa
markocic <markokocic2000@gmail.com>
2024-04-10 00:08:07 +0200

    add app logo to header

--- app/src/main/java/raf/rma/catalist/core/compose/Header.kt
+++ app/src/main/java/raf/rma/catalist/core/compose/Header.kt
@@ -1,9 +1,12 @@
 package raf.rma.catalist.core.compose
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -14,6 +17,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -24,7 +28,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import raf.rma.catalist.R
 import raf.rma.catalist.core.theme.onBackground
 import raf.rma.catalist.core.theme.primaryText
 
@@ -35,7 +42,8 @@ fun Header(
     onBack: () -> Unit,
     onSearch: () -> Unit,
 ) {
-    FlowRow(
+    FlowColumn(
+        maxItemsInEachColumn = 1,
         modifier = Modifier
             .fillMaxWidth()
             .background(onBackground)
@@ -43,20 +51,20 @@ fun Header(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalArrangement = Arrangement.Center,
     ) {
-        Column {
             IconButton(onClick = { onBack() }) {
                 Icon(
-                        imageVector = Icons.Filled.ArrowBack,
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = "back",
                         tint = primaryText,
                         modifier = Modifier.size(24.dp),
                     )
             }
-        }
-        Column {
-            Text(text = "Catalist", color = primaryText)
-        }
-        Column {
+            Image(
+                painter = painterResource(id = R.drawable.catalist_logo),
+                contentDescription = "Catalist logo",
+                modifier = Modifier
+                    .size(40.dp)
+            )
             IconButton(onClick = { onSearch() }) {
                 Icon(
                     imageVector = Icons.Filled.Search,
@@ -64,9 +72,7 @@ fun Header(
                     tint = primaryText,
                     modifier = Modifier.size(24.dp)
                 )
-            }
         }
 
     }
-
 }
