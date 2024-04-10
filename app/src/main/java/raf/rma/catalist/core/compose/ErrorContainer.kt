24eb46079c09e8dca8e0c6ef941ba2312dca7251
markocic <markokocic2000@gmail.com>
2024-04-10 18:52:41 +0200

    style error container component

--- app/src/main/java/raf/rma/catalist/core/compose/ErrorContainer.kt
+++ app/src/main/java/raf/rma/catalist/core/compose/ErrorContainer.kt
@@ -5,9 +5,13 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import raf.rma.catalist.core.theme.Typography
 import raf.rma.catalist.core.theme.primaryText
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -18,13 +22,16 @@ fun ErrorContainer(
     FlowColumn(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(),
+            .fillMaxHeight()
+            .padding(horizontal = 64.dp),
         horizontalArrangement = Arrangement.Center,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = error.message ?: "Unexpected error occurred.",
-            color = primaryText
+            color = primaryText,
+            textAlign = TextAlign.Center,
+            style = Typography.titleMedium
         )
     }
 
