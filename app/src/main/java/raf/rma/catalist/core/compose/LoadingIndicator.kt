ef75bf6a2c7cf1aea19cced576b864d3c5784288
markocic <markokocic2000@gmail.com>
2024-04-10 17:45:56 +0200

    center loading indicator horizontally and vertically

--- app/src/main/java/raf/rma/catalist/core/compose/LoadingIndicator.kt
+++ app/src/main/java/raf/rma/catalist/core/compose/LoadingIndicator.kt
@@ -1,19 +1,36 @@
 package raf.rma.catalist.core.compose
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowColumn
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun IndeterminateCircularIndicator() {
 
-    CircularProgressIndicator(
-        modifier = Modifier.width(32.dp),
-        color = MaterialTheme.colorScheme.secondary,
-        trackColor = MaterialTheme.colorScheme.surfaceVariant,
-    )
+    FlowRow(
+        verticalArrangement = Arrangement.Center,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(32.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    }
 }
